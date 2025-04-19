3d6f3577a93bc8248fa1d256672693489d41d4c6
Kunal Gambhir <129669697+kunalproject@users.noreply.github.com>
2025-04-19 23:38:05 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <div className={styles.content}>
         <div className={styles.leftSection}>
           <div className={styles.imageContainer}>
-            <img src={portfolioData.profilePic} alt={portfolioData.name} className={styles.image} loading="lazy" />
+            <img src={portfolioData.profilePic} alt={"https://res.cloudinary.com/dmmwlsock/image/upload/v1745085967/profile_aur9se.png"} className={styles.image} loading="lazy" />
           </div>
           <h1 className={styles.name}>{portfolioData.name}</h1>
         </div>
